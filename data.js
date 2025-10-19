3ee99ab45bfc30c0d022603479f9d813797774d9
BZR <pedro.belalcazar@upec.edu.ec>
2025-10-18 22:51:10 -0500

    Add product data for streaming services
    Define an array of products with details for each service.

---
+++ data.js
@@ -0,0 +1,64 @@
+// js/data.js
+
+// Importante: Reemplaza XXXXXXXXXX con tu número de WhatsApp
+const WHATSAPP_NUMBER = '0963908286'; 
+
+const productos = [
+    {
+        logo: 'netflix.png', // Asume que tienes este logo
+        nombre: 'Netflix Genérico',
+        plan: '1 Pantalla',
+        precio: '2.60', // Solo el número, el script añade el símbolo
+        descripcion: 'Ultra HD 4K, solicita código de hogar y puede tener interrumpciones',
+        cta_mensaje: 'Hola, me interesa el Plan de Netflix Genérico ($2.60/mes).'
+    },
+   {
+        logo: 'netflix.png', // Asume que tienes este logo
+        nombre: 'Netfl1x (Perfil Extra)',
+        plan: '1 Pantalla',
+        precio: '3.75', // Solo el número, el script añade el símbolo
+        descripcion: 'Ultra HD 4K, sin interrumpciones, un único perfil',
+        cta_mensaje: 'Hola, me interesa el Plan de Netflix - Perfil Extra ($3.75/mes).'
+    },
+    {
+        logo: 'disney.png',
+        nombre: 'D1sney+ PREMIUM',
+        plan: '1 Pantalla',
+        precio: '2.75',
+        descripcion: 'Todo Disney, Pixar, Marvel y el contenido deportivo y adulto de Star+.',
+        cta_mensaje: 'Hola, me interesa el Plan D1sney ($2.75/mes).'
+    },
+    {
+        logo: 'hbo.png',
+        nombre: 'HBO Max',
+        plan: '1 Dispositivo',
+        precio: '2,50',
+        descripcion: 'Series exclusivas de HBO, Warner Bros y el catálogo de DC. Gran calidad.',
+        cta_mensaje: 'Hola, me interesa el Plan HBO Max ($2.50/mes).'
+    },
+    {
+        logo: 'spotify.png',
+        nombre: 'Spotify',
+        plan: '1 Dispositivo',
+        precio: '3x1mes- 8,50x3meses- 15,50x6meses- 25x1año',
+        descripcion: 'Música sin límites, sin anuncios y modo sin conexión. Para un perfil.',
+        cta_mensaje: 'Hola, me interesa el Plan Spotify).'
+    },
+  {
+        logo: 'prime.png',
+        nombre: 'Pr1me V1deo',
+        plan: '1 Dispositivo',
+        precio: '2,50,
+        descripcion: 'Películas y series actuales, Acción, Comedia y mucha diversión',
+        cta_mensaje: 'Hola, me interesa el Plan de Prime Video ($2.50/mes).'
+    },
+      {
+        logo: 'flujo.png',
+        nombre: 'Flujo TV',
+        plan: '1 Dispositivo',
+        precio: '2,75,
+        descripcion: 'Canales de TV, Series, Películas y más',
+        cta_mensaje: 'Hola, me interesa el Plan de Flujo TV.'
+    }
+    // ¡Añade más productos aquí!
+];
