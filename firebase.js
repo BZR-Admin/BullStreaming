519fc31be24bef4a5520ea96bc5321798fee7060
BZR <pedro.belalcazar@upec.edu.ec>
2026-06-16 15:25:00 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,10 +3,14 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyB4VEJxsxMWK0DFlu0jST6cBQBlHFUk0-g",
+  authDomain: "bd-bull-streaming.firebaseapp.com",
+  databaseURL: "https://bd-bull-streaming-default-rtdb.firebaseio.com",
+  projectId: "bd-bull-streaming",
+  storageBucket: "bd-bull-streaming.firebasestorage.app",
+  messagingSenderId: "151325862722",
+  appId: "1:151325862722:web:a7e1613209018ed761b6db",
+  measurementId: "G-7FYT9VY081"
 };
 
 const app = initializeApp(firebaseConfig);
