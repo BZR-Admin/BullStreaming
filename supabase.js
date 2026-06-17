29755311bca7e61fc360339e4c1d741558ef4713
BZR <pedro.belalcazar@upec.edu.ec>
2026-06-17 17:27:53 -0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 const SUPABASE_URL = "https://tsjooishylasseijkard.supabase.co";
-const SUPABASE_KEY = "TU_ANON_KEY";
+const SUPABASE_KEY = "sb_publishable_i4drihcN_x3GTKpm3Ayptg_Pp3LzWPf";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
